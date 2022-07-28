664a6104c7bbfb63ae851beca8f8c27c8b06b311
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-07-28 20:19:35 +0200

    output

--- png_tif_convert.py
+++ png_tif_convert.py
@@ -5,7 +5,7 @@ import imageio
 
 
 inputdir = Path("/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/")
-outputdir = Path("/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/raw/")
+outputdir = "/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/raw/"
 Path(outputdir).mkdir(exist_ok=True)
 pattern = '*.png'
 files = list(inputdir.glob(pattern))
