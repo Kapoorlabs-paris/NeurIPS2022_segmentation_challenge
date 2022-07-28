a9902b93ce1541f5af09d664d4cd8c255c9e3c84
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-07-28 20:07:52 +0200

    glob and path

---
+++ png_tif_convert.py
@@ -0,0 +1,14 @@
+from os import mkdir
+from pathlib import Path
+from tifffile import imread, imwrite
+import imageio
+
+
+inputdir = Path("/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/")
+outputdir = Path("/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/raw/")
+Path(outputdir).mkdir(exist_ok=True)
+pattern = '*.png'
+files = list(inputdir.glob(pattern))
+for file in files:
+    image = imageio(file)
+    print(image.shape)
