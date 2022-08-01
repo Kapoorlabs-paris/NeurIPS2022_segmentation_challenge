610a684a22fa79ae040d454e3c6b71db8bef8cc0
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-01 16:03:06 +0200

    pôsix

--- png_tif_convert.py
+++ png_tif_convert.py
@@ -1,14 +1,25 @@
 import os
 from pathlib import Path
-from tifffile import imwrite
+from tifffile import imwrite, imread
 import imageio
+import numpy as np
+from scipy import misc
 
 
 inputdir = Path("/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/")
 outputdir = "/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegData/Train_Labeled/raw/"
 Path(outputdir).mkdir(exist_ok=True)
-pattern = '*.png'
+pattern = '*.tif'
 files = list(inputdir.glob(pattern))
 for file in files:
-    image = imageio.imread(file)
-    imwrite(outputdir + '/' + os.path.splitext(file.name)[0] + '.tiff', image)
+
+    image = imread(file) #imageio.imread(file)
+    print('in', image.shape)
+    if(len(image.shape)==2):
+          newimage = np.zeros([image.shape[0], image.shape[1], 3]) 
+          for i in range(3): 
+              newimage[:,:,i] = image
+    else:
+        newimage = image 
+    print(newimage.shape)
+    imwrite(outputdir + '/' + os.path.splitext(file.name)[0] + '.tiff', newimage)
