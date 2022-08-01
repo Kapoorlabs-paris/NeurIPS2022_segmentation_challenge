3775dc01170aa6c1669e17fa3fae0c9136b7be4f
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-01 19:08:53 +0200

    hydra code

--- neurips/train_model_hydra.py
+++ neurips/train_model_hydra.py
@@ -14,31 +14,32 @@ def main(cfg : NeurIPSConfig):
 
 
 
-            npz_filename = args.npz_filename
+            
+            npz_filename = cfg.files.npz_filename
 
-            model_name = args.model_name
+            model_name = cfg.files.model_name
 
-            raw_dir = 'raw/'
-            real_mask_dir = 'real_mask/' 
-            binary_mask_dir = 'binary_mask/'
-            binary_erode_mask_dir = 'binary_erode_mask/'
+            raw_dir = cfg.paths.raw_dir
+            real_mask_dir = cfg.paths.real_mask_dir 
+            binary_mask_dir = cfg.paths.binary_mask_dir
+            binary_erode_mask_dir = cfg.paths.binary_erode_mask_dir
 
 
 
             #Network training parameters
-            depth = args.depth
-            epochs = args.epoch
-            learning_rate = args.learning_rate
-            batch_size = args.batch_size
-            patch_x = args.patch_x
-            patch_y = args.patch_y
-            kern_size = args.kern_size
-            n_patches_per_image = args.n_patches_per_image
-            n_rays = args.n_rays
-            startfilter = args.startfilter
-            validation_split = args.validation_split
-            n_channel_in = args.n_channel_in
-            pattern = args.pattern
+            depth = cfg.params.depth
+            epochs = cfg.params.epoch
+            learning_rate = cfg.params.learning_rate
+            batch_size = cfg.params.batch_size
+            patch_x = cfg.params.patch_x
+            patch_y = cfg.params.patch_y
+            kern_size = cfg.params.kern_size
+            n_patches_per_image = cfg.params.n_patches_per_image
+            n_rays = cfg.params.n_rays
+            startfilter = cfg.params.startfilter
+            validation_split = cfg.params.validation_split
+            n_channel_in = cfg.params.n_channel_in
+            pattern = cfg.params.pattern
 
 
             use_gpu_opencl = True
@@ -49,31 +50,31 @@ def main(cfg : NeurIPSConfig):
             train_seed_unet = True
 
 
-SmartSeeds2D(base_dir = base_dir, 
-            npz_filename = npz_filename, 
-            model_name = model_name, 
-            model_dir = model_dir,
-            raw_dir = raw_dir,
-            real_mask_dir = real_mask_dir,
-            binary_mask_dir = binary_mask_dir,
-            binary_erode_mask_dir = binary_erode_mask_dir,
-            n_channel_in = n_channel_in,
-            load_data_sequence = load_data_sequence, 
-            validation_split = validation_split, 
-            n_patches_per_image = n_patches_per_image, 
-            generate_npz = generate_npz, 
-            train_unet = train_unet, 
-            train_star = train_star, 
-            train_seed_unet = train_seed_unet,
-            patch_x= patch_x, 
-            patch_y= patch_y, 
-            use_gpu = use_gpu_opencl,  
-            batch_size = batch_size, 
-            depth = depth, 
-            pattern = pattern,
-            kern_size = kern_size, 
-            startfilter = startfilter, 
-            RGB = True,
-            n_rays = n_rays, 
-            epochs = epochs, 
-            learning_rate = learning_rate)
+            SmartSeeds2D(base_dir = base_dir, 
+                        npz_filename = npz_filename, 
+                        model_name = model_name, 
+                        model_dir = model_dir,
+                        raw_dir = raw_dir,
+                        real_mask_dir = real_mask_dir,
+                        binary_mask_dir = binary_mask_dir,
+                        binary_erode_mask_dir = binary_erode_mask_dir,
+                        n_channel_in = n_channel_in,
+                        load_data_sequence = load_data_sequence, 
+                        validation_split = validation_split, 
+                        n_patches_per_image = n_patches_per_image, 
+                        generate_npz = generate_npz, 
+                        train_unet = train_unet, 
+                        train_star = train_star, 
+                        train_seed_unet = train_seed_unet,
+                        patch_x= patch_x, 
+                        patch_y= patch_y, 
+                        use_gpu = use_gpu_opencl,  
+                        batch_size = batch_size, 
+                        depth = depth, 
+                        pattern = pattern,
+                        kern_size = kern_size, 
+                        startfilter = startfilter, 
+                        RGB = True,
+                        n_rays = n_rays, 
+                        epochs = epochs, 
+                        learning_rate = learning_rate)
