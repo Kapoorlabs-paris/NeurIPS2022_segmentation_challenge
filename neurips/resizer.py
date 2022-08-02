e3061d79f566baa17a8948f2dd7a0c6d0723dcce
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-02 18:10:06 +0200

    res

--- neurips/resizer.py
+++ neurips/resizer.py
@@ -10,17 +10,19 @@ outputdir = "/gpfsstore/rech/jsy/uzj81mi/Segmentation_challenge/NeurIPS_CellSegD
 Path(outputdir).mkdir(exist_ok=True)
 pattern = '*.tiff'
 minsize = (256,256)
-
+shape = (256,256,3)
 files = list(inputdir.glob(pattern))
 nthreads = os.cpu_count()
 def resizer(file):
     image = imread(file)
-    newimage = np.zeros(minsize)
+    newimage = np.zeros(shape)
+    print(image.shape, newimage.shape)
     if image.shape[0] < minsize[0] and image.shape[1] < minsize[1]:
+          print('in')
           for i in range(0, image.shape[2]):
              newimage[:,:,i] =cv2.resize(
                 image[:,:,i].astype('float32'), minsize)
-    return newimage, file.name   
+    return newimage, file.name
 with concurrent.futures.ThreadPoolExecutor(max_workers = nthreads) as executor:
      futures = []
      for fname in files:
@@ -28,3 +30,4 @@ with concurrent.futures.ThreadPoolExecutor(max_workers = nthreads) as executor:
      for future in concurrent.futures.as_completed(futures):
                    newimage, name = future.result()
                    imwrite(outputdir + '/' + os.path.splitext(name)[0] + '.tiff', newimage)
+~               
