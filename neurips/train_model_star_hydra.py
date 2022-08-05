5c09e96bd3f31fe2cd28bb8ec5984c6025656b05
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-05 23:55:28 +0200

    conf

--- neurips/train_model_star_hydra.py
+++ neurips/train_model_star_hydra.py
@@ -14,7 +14,7 @@ def main(config : NeurIPSStarConfig):
             model_dir = config.paths.model_dir
             model_name = config.files.model_name
             raw_dir = config.paths.raw_dir
-            real_mask_dir = config.paths.real_mask_dir 
+            real_mask_dir = config.paths.real_mask_dir
             binary_mask_dir = config.paths.binary_mask_dir
             binary_erode_mask_dir = config.paths.binary_erode_mask_dir
             #Network training parameters
@@ -25,15 +25,12 @@ def main(config : NeurIPSStarConfig):
             patch_x = config.params.patch_x
             patch_y = config.params.patch_y
             kern_size = config.params.kern_size
-            n_rays = config.params.n_rays
             startfilter = config.params.startfilter
             validation_split = config.params.validation_split
             n_channel_in = config.params.n_channel_in
             pattern = config.params.pattern
-            train_star = config.params.train_star
-            use_gpu_opencl = config.params.use_gpu_opencl
-            load_data_sequence = config.params.load_data_sequence
             RGB = config.params.RGB
+            n_rays = config.params.n_rays
 
             SmartSeeds2D(base_dir = base_dir, 
                         model_name = model_name, 
@@ -43,11 +40,11 @@ def main(config : NeurIPSStarConfig):
                         binary_mask_dir = binary_mask_dir,
                         binary_erode_mask_dir = binary_erode_mask_dir,
                         n_channel_in = n_channel_in,
-                        load_data_sequence = load_data_sequence, 
+                        load_data_sequence = False, 
                         validation_split = validation_split, 
                         patch_x= patch_x, 
                         patch_y= patch_y, 
-                        use_gpu = use_gpu_opencl,  
+                        use_gpu = False,  
                         batch_size = batch_size, 
                         depth = depth, 
                         pattern = pattern,
